ecb9ebe7bde7449b5c2e1acefb53f18030014fb2
Theoalban <kemajoutheodore@hotmail.com>
2023-11-11 21:53:17 -0600

    module

---
+++ app1/main.tf
@@ -0,0 +1,20 @@
+module "server1" {
+    source = "../modules/ec2"
+    ami = "ami-05c13eab67c5d8861"
+    region = "us-east-1"
+    instance_type = "t3.small"
+    name = "DEV-from module"
+  
+}
+output "public_ip" {
+    value = module.server1.public-ip
+  
+}
+output "public_dns" {
+    value = module.server1.public-dns
+  
+}
+output "instance_id" {
+    value = module.server1.instance-id
+  
+}
