391859b7df26facfd6b1f32803ff53ca69a8f0b3
Theoalban <kemajoutheodore@hotmail.com>
2023-11-25 15:11:26 -0600

    change on aap7

--- app7-provisioner-null-ressource/main.tf
+++ app7-provisioner-null-ressource/main.tf
@@ -3,41 +3,39 @@ resource "aws_iam_group" "group1" {
 }
 
 
-
 resource "aws_instance" "name" {
- ami =  data.aws_ami.ami1.id
- instance_type = "t2.micro"
- vpc_security_group_ids = [aws_security_group.ssh.id]
- key_name = aws_key_pair.ec2_key.key_name
+  ami           = data.aws_ami.ami1.id
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.ec2_key.key_name
 
- depends_on = [ aws_key_pair.ec2_key, aws_iam_group.group1 ]
+  depends_on = [aws_key_pair.ec2_key, aws_iam_group.group1]
 
- connection {
-   type = "ssh"
-   user = "ec2-user"
-   private_key = "week12b.pem"
-   host = self.public_ip
- }
- provisioner "local-exec" {
-   command = "echo hello"
- }
- provisioner "remote-exec" {
-   inline = [ 
-    "sudo yum update -y",
-    "mkdir serge",
-    "pwd",
-    "nproc"
-    ]   
- }
- provisioner "file" {
-   source = "week12b.pem"
-   destination = "/tmp/w.pem"
- }
- 
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = "week12b.pem"
+    host        = self.public_ip
+  }
+  provisioner "local-exec" {
+    when    = destroy
+    command = "echo hello"
+  }
+  provisioner "remote-exec" {
+    when = destroy
+    inline = [
+      "sudo yum update -y",
+      "mkdir theo",
+      "pwd",
+      "nproc",
+    ]
+  }
+  provisioner "file" {
+    when = destroy
+    source      = "week12b.pem"
+    destination = "/tmp"
+  }
+
 }
-
-
-
 
 
 
